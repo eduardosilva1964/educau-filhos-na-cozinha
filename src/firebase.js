b0d0f29541c04b76fd5b041c0a9bba292411cbc2
EduCau <eduardosilva4@gmail.com>
2026-05-26 09:31:14 -0300

    protege chaves Firebase

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCzwtEC0AgiS9zWm0le4N-G-e1Z7hn1bLU",
+  apiKey: import.meta.env.VITE_FB_API_KEY,
   authDomain: "educau-filhos-na-cozinha.firebaseapp.com",
-  projectId: "educau-filhos-na-cozinha",
+  projectId: import.meta.env.VITE_FB_PROJECT_ID,
   storageBucket: "educau-filhos-na-cozinha.firebasestorage.app",
-  messagingSenderId: "438524728051",
-  appId: "1:438524728051:web:99721e7cf7154001109d86"
+  messagingSenderId: import.meta.env.VITE_FB_SENDER_ID,
+  appId: import.meta.env.VITE_FB_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
